d48cfa0f70695436ebf463339896e2dcee9bbb5d
tigawanna <denniskinuthiaw@gmail.com>
2022-07-25 14:20:54 +0300

    fixes

--- src/components/Navigation/Toolbar/Toolbar.tsx
+++ src/components/Navigation/Toolbar/Toolbar.tsx
@@ -16,30 +16,27 @@ const [open, setOpen] = useState(false)
 const actionTs=()=>{updateToken(undefined)}
 
 return (
- <div className='w-[100%] bg-slate-500 h-16 max-h-16'>
+ <div className='w-[100%] bg-slate-500 h-[60px] max-h-[50px] flex-center'>
 <IconContext.Provider value={{ size: "25px", className: "table-edit-icons" }} >
 
 {open?<Consent setOpen={setOpen} message={"Sign Out?"} action={actionTs}/>:null}
 
  <div className='flex flex-grow flex-3 text-lg font-bold'>
-     <div className='m-1 w-full p-3 bg-slate-400 flex-center'>
+     <div className='w-full p-3 bg-slate-400 flex-center'>
      <Link to="/"><GrHome /></Link>
      </div>
-     <div className='m-1 w-full p-3 bg-slate-300 flex-center'>
+     {/* <div className='m-1 w-full p-3 bg-slate-300 flex-center'>
      <Link to="/profile">Profile</Link>
      </div>  
       <div className='m-1 w-full p-3 bg-slate-300 flex-center'>
      <Link to="/user">User</Link>
-     </div>
-
-
-
-     <div 
+     </div> */}
+  <div 
       onClick={()=>setOpen(true)}
-     className='m-1   border-slate-900 border-2 rounded-md hover:bg-slate-700 flex-center'>
+     className='h-[45px]  hover:bg-slate-700 flex-center m-1'>
     {/* <FaUserCircle /> */}
     <img
-     className='max-h-16 w-fit m-1'
+     className='h-[100%] w-fit rounded-md m-1'
      src={user?.avatar_url}
      alt=""
     />
