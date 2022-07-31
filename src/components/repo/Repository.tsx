923592f54e162c8039a2d0e24ad9e080e269556f
tigawanna <denniskinuthiaw@gmail.com>
2022-07-31 16:01:32 +0300

    yseRepo hook

--- src/components/repo/Repository.tsx
+++ src/components/repo/Repository.tsx
@@ -37,11 +37,11 @@ repo:RepoType
 
 export const RepoCard: React.FC<RepoCardProps> = ({repo}) => {
 return (
-    <div className="h-[25%] w-[95%] md:w-[40%] lg:w-[30%] p-5 flex 
+    <div className="h-52 w-[95%] md:w-[40%] lg:w-[30%] p-5 flex-col 
     ustify-between items-center shadow-lg shadow-slate-300  m-2 border-black border-2 rounded-md">
         <div 
         onClick={() => {}}
-        className=" flex-col items-center  justify-between  cursor-pointer h-full w-full">
+        className=" flex-col items-center  justify-between  cursor-pointer h-[90%] w-full">
      
            <div className="text-[25px] font-semibold md:text-xl md:font-bold  break-all ">
             {repo?.name}
@@ -49,19 +49,18 @@ return (
           <div className="text-[20px] font-semibold md:text-sm  break-all ">
             {repo?.language}
           </div>
-          <div className="text-[14px] md:text-sm  break-all ">
+          <div className="text-[14px] md:text-sm  break-all max-h-16 h-full overflow-y-clip">
             {repo?.description}
           </div>
-          <div className="w-full text-[15px] :text-sm  flex justify-between">
-     
-          <div className="text-[12px] font-medium">
-            last update: {dayjs(repo?.updated_at).format('DD/MM/YYYY')}</div>
-          
-          </div>
-          <div className='flex-center'><BiGitRepoForked/> {repo?.forks_count}</div>
-          
 
+   
         </div>
+        <div className="w-full text-[15px] text-sm  flex justify-between ">
+           <div className="text-[12px] font-medium">
+            last update: {dayjs(repo?.updated_at).format('DD/MM/YYYY')}</div>
+            <div className='flex-center'><BiGitRepoForked/> {repo?.forks_count}</div>
+            <div className='flex-center'><BiGitRepoForked/> {repo?.visibility}</div>
+          </div>
 
  </div>
 );
