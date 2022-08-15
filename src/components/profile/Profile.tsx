d08064ca5b09c369ba121f8f0fdf0f3fffb4aba4
tigawanna <denniskinuthiaw@gmail.com>
2022-08-15 15:29:49 +0300

    fix

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -19,7 +19,7 @@ const user = dev as MainAuthedUser
 
 //console.log(dev.login)
 // const followers_url ="https://api.github.com/user/following"
-const following_url =`https://api.github.com/users/${user?.login}/following`
+const following_urls =`https://api.github.com/users/${user?.login}/following`
 
 
 
@@ -37,7 +37,7 @@ return (
 
 <div className='w-full  h-fit  flex-col p-2 bg-slate-200 font-sans'>
 <div className='text-lg font-bold'>Following</div>
-<Following token={token} url={following_url} user={user} ogUser={ogUser}/>
+<Following token={token} url={following_urls} user={user} ogUser={ogUser}/>
 </div>
 
 
