2d5fd39dca0a13a0fe7f006322292880a84aedf9
tigawanna <denniskinuthiaw@gmail.com>
2022-08-17 13:18:23 +0300

    repo types

--- src/components/repo/utils/type.ts
+++ src/components/repo/utils/type.ts
@@ -1,3 +1,57 @@
-export interface REPO{
-    
+export interface ROOTREPO {
+  pages: Page[];
+  pageParams: any[];
+}
+
+export interface Page {
+  user: User;
+}
+
+export interface User {
+  login: string;
+  repositories: Repositories;
+}
+
+export interface Repositories {
+  edges: REPOEDGE[];
+  totalCount: number;
+  pageInfo: PageInfo;
+}
+
+export interface REPOEDGE {
+  node: Node;
+  cursor: string;
+}
+
+export interface REPONODE {
+  id: string;
+  name: string;
+  description?: string;
+  pushedAt: string;
+  diskUsage: number;
+  url: string;
+  visibility: string;
+  forkCount: number;
+  languages: Languages;
+}
+
+export interface Languages {
+  edges: LANGUAGEEDGE[];
+}
+
+export interface LANGUAGEEDGE {
+  node: LANGUAGENODE;
+}
+
+export interface LANGUAGENODE {
+  id: string;
+  color: string;
+  name: string;
+}
+
+export interface PageInfo {
+  startCursor: string;
+  endCursor: string;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
 }
