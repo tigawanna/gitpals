b1bf049fc0de2754537e445085ca68564a14ed9e
tigawanna <denniskinuthiaw@gmail.com>
2022-08-22 07:33:54 +0300

    gql get repo commit history query

--- src/components/repo/utils/query.ts
+++ src/components/repo/utils/query.ts
@@ -44,3 +44,102 @@ export const REPOS = gql`
            }
          }
        `;
+
+
+const RECENTREPOCOMMIT = gql`
+  query getRepoRecentCommit {
+    repository(owner: "tigawanna", name: "gitpals") {
+      defaultBranchRef {
+        name
+        target {
+          ... on Commit {
+            history(first: 1) {
+              edges {
+                node {
+                  committedDate
+                  author {
+                    name
+                  }
+                  message
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+
+export const REPOWITHPREVCOMMIT = gql`
+         query getRepos($name: String!, $first: Int, $after: String) {
+           user(login: $name) {
+             login
+             repositories(
+               after: $after
+               first: $first
+               orderBy: { field: PUSHED_AT, direction: DESC }
+             ) {
+               edges {
+                 node {
+                   id
+                   name
+                   description
+                   pushedAt
+                   diskUsage
+
+                   url
+                   visibility
+                   forkCount
+                   refs(
+                     refPrefix: "refs/heads/"
+                     orderBy: { direction: DESC, field: TAG_COMMIT_DATE }
+                     first: 2
+                   ) {
+                     edges {
+                       node {
+                         name
+                         id
+                         target {
+                           ... on Commit {
+                             history(first: 1) {
+                               edges {
+                                 node {
+                                   committedDate
+                                   author {
+                                     name
+                                   }
+                                   message
+                                 }
+                               }
+                             }
+                           }
+                         }
+                       }
+                     }
+                   }
+
+                   languages(first: $first) {
+                     edges {
+                       node {
+                         id
+                         color
+                         name
+                       }
+                     }
+                   }
+                 }
+                 cursor
+               }
+               totalCount
+               pageInfo {
+                 startCursor
+                 endCursor
+                 hasNextPage
+                 hasPreviousPage
+               }
+             }
+           }
+         }
+       `;
